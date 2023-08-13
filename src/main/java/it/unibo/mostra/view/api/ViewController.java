06cb1c1c0298d0928cd8de224a67d059a0d75bcc
unknown <margherita.balzoni@studio.unibo.it>
2023-08-13 14:52:23 +0200

    Created ViewController interface

---
+++ src/main/java/it/unibo/mostra/view/api/ViewController.java
@@ -0,0 +1,9 @@
+package main.java.it.unibo.mostra.view.api;
+/**
+ * Interface for the viewController.
+ */
+public interface ViewController {
+    public setHomeView();
+
+    public setLoginView();
+}
