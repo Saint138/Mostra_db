ebb103391264c4e539368a16926db4d2c77b989b
gioele santi <gioele.santi2@studio.unibo.it>
2023-08-13 14:42:19 +0200

    created ConnectionManager

---
+++ src/main/java/it/unibo/mostra/db/ConnectionManager.java
@@ -0,0 +1,44 @@
+package it.unibo.mostra.db;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ * Class that manages the connection to the database.
+ */
+public class ConnectionManager {
+
+    private final String user, password, dbname;
+
+    /**
+     * Constructor.
+     */
+    public ConnectionManager(String user, String password) {
+        this.user = user;
+        this.password = password;
+        this.dbname = "Mostradb";
+    }
+
+    /**
+     * Creates a connection to the database.
+     * 
+     * @return the connection to the database.
+     * @throws SQLException if the connection cannot be established.
+     * @throws ClassNotFoundException if the driver cannot be found.
+     */
+    public Connection getSQLConnection() {
+
+        final String dbLink = "jdbc:mysql://localhost:3306/" + this.dbname;
+
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            return DriverManager.getConnection(
+                    dbLink, this.user, this.password);
+        } catch (final SQLException e) {
+            throw new IllegalStateException("Cannot connect the database!", e);
+        } catch (final ClassNotFoundException e) {
+            throw new IllegalStateException("Cannot find the driver in the classpath!", e);
+        }
+    }
+}
