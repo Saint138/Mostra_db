3029a1cfd6fb3dea6a3f9d4af7f7fa2a5722d08a
unknown <margherita.balzoni@studio.unibo.it>
2023-08-16 22:49:24 +0200

    created query in QueryRecensione class

--- src/main/java/it/unibo/mostra/db/query/QueryRecensione.java
+++ src/main/java/it/unibo/mostra/db/query/QueryRecensione.java
@@ -20,7 +20,7 @@ public class QueryRecensione {
         final String query = "SELECT  V.nome,V.cognome, R.data_recensione, R.valutazione, R.commento"
                             + "FROM Recensione R"
                             + "JOIN Visitatore V on R.CF = V.CF"
-                            + "WHERE codice_mostra= ?";
+                            + "WHERE R.codice_mostra= ?";
         try (PreparedStatement stmt = this.connection.prepareStatement(query)) {
             stmt.setString(1, codiceMostra);
             final ResultSet rs = stmt.executeQuery();
@@ -29,6 +29,74 @@ public class QueryRecensione {
             while(rs.next()){
                 list.add(new Recensione(rs.getString("V.nome"), rs.getString("V.cognome"),
                                                rs.getString("R.Valutazione"), rs.getString("R.commento")));
+            }
+            return list;
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return null;
+        }
+
+    }
+
+    public ObservableList<Recensione> visulizzaRecensioniUtente(String CF){
+        final String query = "SELECT  V.nome,V.cognome, R.data_recensione, R.valutazione, R.commento"
+                            + "FROM Recensione R"
+                            + "JOIN Visitatore V on R.CF = V.CF"
+                            + "WHERE V.CF= ?";
+                            
+        try (PreparedStatement stmt = this.connection.prepareStatement(query)) {
+            stmt.setString(1, CF);
+            final ResultSet rs = stmt.executeQuery();
+
+            final ObservableList<Recensione> list = FXCollections.observableArrayList();
+            while(rs.next()){
+                list.add(new Recensione(rs.getString("V.nome"), rs.getString("V.cognome"),
+                                               rs.getString("R.Valutazione"), rs.getString("R.commento")));
+            }
+            return list;
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return null;
+        }
+
+    }
+
+    public ObservableList<Recensione> visulizzaRecensioniPerData(String data){
+        final String query = "SELECT  V.nome,V.cognome, M.nome, R.valutazione, R.commento"
+                            + "FROM Recensione R"
+                            + "JOIN Visitatore V on R.CF = V.CF"
+                            + "JOIN  Mostra M on R.codice_mostra = M.codice_mostra"
+                            + "WHERE R.data_recensione = ?";
+                            
+        try (PreparedStatement stmt = this.connection.prepareStatement(query)) {
+            stmt.setString(1, data);
+            final ResultSet rs = stmt.executeQuery();
+
+            final ObservableList<Recensione> list = FXCollections.observableArrayList();
+            while(rs.next()){
+                list.add(new Recensione(rs.getString("V.nome"), rs.getString("V.cognome"),rs.getString("M.nome"),
+                                               rs.getString("R.Valutazione"), rs.getString("R.commento")));
+            }
+            return list;
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return null;
+        }
+
+    }
+
+    public ObservableList<Recensione> visulizzaMediaRecensioni(){
+        final String query = "SELECT M.nome, COUNT( R.codice_recensione) as numero_recensioni, AVG(R.valutazione) as media"
+                            + "FROM Mostra M"
+                            + "JOIN Recensione R on M.codice_mostra = R.codice_mostra"
+                            + "GROUP BY M.nome";
+
+        try (PreparedStatement stmt = this.connection.prepareStatement(query)) {
+            final ResultSet rs = stmt.executeQuery();
+
+            final ObservableList<Recensione> list = FXCollections.observableArrayList();
+            while(rs.next()){
+                list.add(new Recensione(rs.getString("M.nome"), rs.getString("numero_recensioni"),rs.getString("media")));
             }
             return list;
         } catch (SQLException e) {
