93b43c8605df6e0c57cf991142474d92e24453f1
unknown <margherita.balzoni@studio.unibo.it>
2023-08-16 17:45:32 +0200

    created Opera class

---
+++ src/main/java/it/unibo/mostra/db/entity/Opera.java
@@ -0,0 +1,44 @@
+package it.unibo.mostra.db.entity;
+
+public class Opera {
+    private String nomeArte;
+    private String nomeOpera;
+    private String codiceVendita;
+    private String annoRealizzazione;
+    private String dimensioni;
+    private String tecnica;
+    private String descrizione;
+
+    public Opera(String nomeArtista, String nomeOpera, String codiceVendita, String annoRealizzazione, String dimensioni, String tecnica, String descrizione ){
+        this.annoRealizzazione = annoRealizzazione;
+        this.codiceVendita = codiceVendita;
+        this.nomeArte = nomeArtista;
+        this.nomeOpera = nomeOpera;
+        this.dimensioni = dimensioni;
+        this.tecnica = tecnica;
+        this.descrizione = descrizione;
+    }
+
+    public String getNomeOpera(){
+        return this.nomeOpera;
+    }
+    public String getNomeArtista(){
+        return this.nomeArte;
+    }
+    public String getCodiceVendita(){
+        return this.codiceVendita;
+    }
+    public String getAnnoRealizzazione(){
+        return this.annoRealizzazione;
+    }
+    public String getDimensioni(){
+        return this.dimensioni;
+    }
+    public String getDescrizione(){
+         return this.descrizione;
+    }
+    public String getTecnica(){
+        return this.tecnica;
+    }
+
+}
