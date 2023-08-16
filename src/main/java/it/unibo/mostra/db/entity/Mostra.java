c4a2dce8768d0c1593653105acbd615b2f290c7a
unknown <margherita.balzoni@studio.unibo.it>
2023-08-16 17:45:02 +0200

    created Mostra class

---
+++ src/main/java/it/unibo/mostra/db/entity/Mostra.java
@@ -0,0 +1,39 @@
+public class Mostra {
+    private String nome;
+    private String città;
+    private Integer numeroOpere;
+    private final String codiceMostra;
+    private Integer valore;
+    private String dataInizio;
+
+    public Mostra(String nome, String città, Integer numeroOpere, String data, String codiceMostra, Integer valore ){
+        this.città = città;
+        this.nome = nome;
+        this.numeroOpere = numeroOpere;
+        this.codiceMostra = codiceMostra;
+        this.valore = valore;
+        this.dataInizio = data;
+    }
+
+    public String getNome(){
+        return nome;
+    }
+    public String getCitta(){
+        return città;
+    }
+    public String getData(){
+        return dataInizio;
+    }
+    public Integer getNumeroOpere (){
+        return numeroOpere;
+    }
+    public String getCodiceMostra(){
+        return codiceMostra;
+    }
+    public Integer getVal(){
+        return valore;
+    }
+
+    
+
+}
