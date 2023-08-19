c548c229befe929ec1c00e1c57d3422b71aac1c4
unknown <margherita.balzoni@studio.unibo.it>
2023-08-19 16:26:12 +0200

    Created method to add a new seller

--- src/main/java/it/unibo/mostra/db/query/QueryFornitore.java
+++ src/main/java/it/unibo/mostra/db/query/QueryFornitore.java
@@ -12,4 +12,23 @@ public class QueryFornitore {
     public QueryFornitore(Connection connection) {
         this.connection = connection;
     }
+
+    //inserire una nuovo fornitore
+  
+    public void addFornitore(String codice_fornitore, String nome, String cognome, String email, String numero_telefono) throws SQLException, SQLIntegrityConstraintViolationException {
+        final String query = "INSERT INTO Mostra (CODICE_FORNITORE, NOME, COGNOME, EMAIL , NUMERO_TELEFONO) "
+                            + "VALUES (?, ?, ?, ?, ?)";
+        try (PreparedStatement stmt = connection.prepareStatement(query)) {
+            stmt.setString(1, codice_fornitore);
+            stmt.setInt(2, nome);
+            stmt.setString(3, cognome);
+            stmt.setString(4, email);
+            stmt.setString(5, numero_telefono);
+            stmt.executeUpdate();
+        } catch (SQLIntegrityConstraintViolationException e) {
+            System.out.println("Fornitore già inserita");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
 }
