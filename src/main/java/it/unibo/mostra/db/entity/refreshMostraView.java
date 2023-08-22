5e620ab11767d4ae990ba7235b2b628b7fb82cbb
gioele santi <gioele.santi2@studio.unibo.it>
2023-08-22 11:48:26 +0200

    aggiunta refreshMostraView

---
+++ src/main/java/it/unibo/mostra/db/entity/refreshMostraView.java
@@ -0,0 +1,38 @@
+package it.unibo.mostra.db.entity;
+
+public class refreshMostraView {
+    private String nome;
+    private String codice_mostra;
+    private String città;
+    private String data_inizio;
+    private String data_fine;
+    
+
+    public refreshMostraView(String nome, String codice_mostra,String città,String data_inizio, String data_fine) {
+        this.nome = nome;
+        this.codice_mostra = codice_mostra;
+        this.città = città;
+        this.data_inizio = data_inizio;
+        this.data_fine = data_fine;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getCodiceMostra() {
+        return codice_mostra;
+    }
+
+    public String getCittà() {
+        return città;
+    }
+    
+    public String getDataInizio() {
+        return data_inizio;
+    }
+
+    public String getDataFine() {
+        return data_fine;
+    }
+}
