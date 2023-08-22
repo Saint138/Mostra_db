a3d80368c7135600cd13782599d2b6ef1b80b438
unknown <margherita.balzoni@studio.unibo.it>
2023-08-22 18:31:21 +0200

    Modificato il metodo addRecensione. Ora genera in modo casuale il codice della recensione

--- src/main/java/it/unibo/mostra/db/query/QueryRecensione.java
+++ src/main/java/it/unibo/mostra/db/query/QueryRecensione.java
@@ -5,11 +5,9 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.SQLIntegrityConstraintViolationException;
-import java.util.List;
 import java.util.Random;
 
 import it.unibo.mostra.db.entity.Recensione;
-import it.unibo.mostra.db.entity.refreshBiglietteria;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 
@@ -24,11 +22,12 @@ public class QueryRecensione {
     }
 
   
-    public void addRecensione(String cod, String Cf, String commento, String codMostra, Integer val) throws SQLException, SQLIntegrityConstraintViolationException {
+    public void addRecensione(String Cf, String commento, String codMostra, Integer val) throws SQLException, SQLIntegrityConstraintViolationException {
         final String query = "INSERT INTO Mostra (CODICE_RECENSIONE, CF, COMMENTO, CODICE_MOSTRA , VALUTAZIONE) "
                             + "VALUES (?, ?, ?, ?, ?)";
+        String cod = 'A'+ Integer.toString(rand.nextInt(1000, 9999));
         try (PreparedStatement stmt = connection.prepareStatement(query)) {
-            stmt.setString(1, cod);
+            stmt.setString(1, cod );
             stmt.setString(2, Cf);
             stmt.setString(3, commento);
             stmt.setString(4, codMostra);
