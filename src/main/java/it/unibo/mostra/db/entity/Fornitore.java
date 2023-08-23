513d3750b44d1de93e86041a1aa01193b4377f8b
unknown <margherita.balzoni@studio.unibo.it>
2023-08-23 22:26:15 +0200

    corretta classe Fornitore, rimuovendo il campo cognome

--- src/main/java/it/unibo/mostra/db/entity/Fornitore.java
+++ src/main/java/it/unibo/mostra/db/entity/Fornitore.java
@@ -3,24 +3,18 @@ package it.unibo.mostra.db.entity;
 public class Fornitore {
     private String codice_fornitore;
     private String nome;
-    private String cognome;
     private String email;
     private String numero_telefono;
 
-    public Fornitore(String codice_fornitore, String nome, String cognome, String email, String numero_telefono) {
+    public Fornitore(String codice_fornitore, String nome, String email, String numero_telefono) {
         this.nome = nome;
         this.codice_fornitore = codice_fornitore;
-        this.cognome = cognome;
         this.email = email;
         this.numero_telefono = numero_telefono;
     }
 
     public String getNome() {
         return nome;
-    }
-
-    public String getCognome() {
-        return cognome;
     }
 
     public String getEmail() {
