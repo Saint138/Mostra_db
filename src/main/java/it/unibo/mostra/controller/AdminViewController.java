c1e11583aa8e1dacc7b2f93fc77f7afe20f60de0
gioele santi <gioele.santi2@studio.unibo.it>
2023-08-23 12:21:08 +0200

    modifiche per funzionamento di admin view

--- src/main/java/it/unibo/mostra/controller/AdminViewController.java
+++ src/main/java/it/unibo/mostra/controller/AdminViewController.java
@@ -1,5 +1,42 @@
 package it.unibo.mostra.controller;
 
+import it.unibo.mostra.db.query.QueryArtista;
+import it.unibo.mostra.db.query.QueryBiglietteria;
+import it.unibo.mostra.db.query.QueryDipendenti;
+import it.unibo.mostra.db.query.QueryFornitore;
+import it.unibo.mostra.db.query.QueryMostra;
+import it.unibo.mostra.db.query.QueryOpera;
+import it.unibo.mostra.db.query.QueryPresenza;
+import it.unibo.mostra.db.query.QueryRecensione;
+import it.unibo.mostra.db.query.QueryRicerca;
+import it.unibo.mostra.db.query.QueryUtente;
+import it.unibo.mostra.db.query.QueryVendita;
+import it.unibo.mostra.db.query.QueryVisita;
+import it.unibo.mostra.view.ViewImpl;
+
 public class AdminViewController {
-    
+
+    private QueryDipendenti queryDipendenti;
+    private QueryMostra queryMostra;
+    private QueryFornitore queryFornitore;
+    private QueryOpera queryOpera;
+    private QueryUtente queryUtente;
+    private QueryVisita queryVisita;
+    private QueryVendita queryVendita;
+    private QueryArtista queryArtista;
+    private QueryPresenza queryPresenza;
+    private ViewImpl view;
+
+    public AdminViewController(ViewImpl view,QueryVisita queryVisita,QueryOpera queryOpera,QueryMostra queryMostra,QueryFornitore queryFornitore,QueryDipendenti queryDipendenti,QueryUtente queryUtente,QueryVendita queryVendita,QueryArtista queryArtista,QueryPresenza queryPresenza){
+        this.view=view;
+        this.queryDipendenti=queryDipendenti;
+        this.queryArtista=queryArtista;
+        this.queryFornitore=queryFornitore;
+        this.queryMostra=queryMostra;
+        this.queryOpera=queryOpera;
+        this.queryUtente=queryUtente;
+        this.queryVisita=queryVisita;
+        this.queryVendita=queryVendita;
+        this.queryPresenza=queryPresenza;
+    }
 }
